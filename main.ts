0531cce509a586d502cb71b37ad07c3ad5de2e1a
hwangbomee <88476660+i33W@users.noreply.github.com>
2025-02-11 17:26:02 +0900

    LCD, 7-Segment 블록 추가

--- main.ts
+++ main.ts
@@ -1,3 +1,475 @@
-basic.forever(function () {
-	
-})
+/**
+* BP Lab Extensions
+*/
+//% weight=1 color=#0fbc11 icon="\uf0ad"
+namespace BPLAB {
+
+    // 그룹 1: LCD 관련
+    // -----------------------------------------------------------------------------
+    let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574 
+    let BK: number      // Backlight control
+    let RS: number      // Command/Data selection
+
+    // LCD register setup
+    function setreg(d: number) {
+        pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
+        basic.pause(1)
+    }
+
+    // Send data through I2C bus
+    function set(d: number) {
+        d = d & 0xF0
+        d = d + BK + RS
+        setreg(d)
+        setreg(d + 4)
+        setreg(d)
+    }
+
+    // Send command
+    function cmd(d: number) {
+        RS = 0
+        set(d)
+        set(d << 4)
+    }
+
+    // Send data
+    function dat(d: number) {
+        RS = 1
+        set(d)
+        set(d << 4)
+    }
+
+    // Auto detect LCD address
+    function AutoAddr() {
+        let k = true
+        let addr = 0x20
+        let d1 = 0, d2 = 0
+        while (k && (addr < 0x28)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+
+        addr = 0x38
+        while (k && (addr < 0x40)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+        else return 0
+    }
+
+    /**
+     * Initialize LCD and set I2C address. PCF8574/PCF8574A address is 39/63
+     * @param Addr LCD i2c address, eg: 0, 39, 63. 0 for auto detection
+     */
+    //% group="LCD"
+    //% blockId="I2C_LCD1620_SET_ADDRESS" block="LCD initialize with Address %addr"
+    //% weight=100 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function LcdInit(Addr: number) {
+        if (Addr == 0) i2cAddr = AutoAddr()
+        else i2cAddr = Addr
+        BK = 8
+        RS = 0
+        cmd(0x33)       // Set 4-bit mode
+        basic.pause(5)
+        set(0x30)
+        basic.pause(5)
+        set(0x20)
+        basic.pause(5)
+        cmd(0x28)       // Set mode
+        cmd(0x0C)
+        cmd(0x06)
+        cmd(0x01)       // Clear screen
+    }
+
+    /**
+     * Display number at specified position on LCD
+     * @param n number to display, eg: 10, 100, 200
+     * @param x LCD column position, eg: 0
+     * @param y LCD row position, eg: 0
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="show number %n|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowNumber(n: number, x: number, y: number): void {
+        let s = n.toString()
+        ShowString(s, x, y)
+    }
+
+    /**
+     * Display string at specified position on LCD
+     * @param s string to display, eg: "Hello"
+     * @param x LCD column position, [0 - 15], eg: 0
+     * @param y LCD row position, [0 - 1], eg: 0
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowString(s: string, x: number, y: number): void {
+        let a: number
+
+        if (y > 0)
+            a = 0xC0
+        else
+            a = 0x80
+        a += x
+        cmd(a)
+
+        for (let i = 0; i < s.length; i++) {
+            dat(s.charCodeAt(i))
+        }
+    }
+
+    /**
+     * Turn on LCD
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_ON" block="turn on LCD"
+    //% weight=81 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function on(): void {
+        cmd(0x0C)
+    }
+
+    /**
+     * Turn off LCD
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_OFF" block="turn off LCD"
+    //% weight=80 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function off(): void {
+        cmd(0x08)
+    }
+
+    /**
+     * Clear all display contents
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_CLEAR" block="clear LCD"
+    //% weight=85 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function clear(): void {
+        cmd(0x01)
+    }
+
+    /**
+     * Turn on LCD backlight
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
+    //% weight=71 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOn(): void {
+        BK = 8
+        cmd(0)
+    }
+
+    /**
+     * Turn off LCD backlight
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="turn off backlight"
+    //% weight=70 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOff(): void {
+        BK = 0
+        cmd(0)
+    }
+
+    /**
+     * Shift display left
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHL" block="Shift Left"
+    //% weight=61 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shl(): void {
+        cmd(0x18)
+    }
+
+    /**
+     * Shift display right
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHR" block="Shift Right"
+    //% weight=60 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shr(): void {
+        cmd(0x1C)
+    }
+
+    // 그룹 2: 7-Segment 관련
+    // -----------------------------------------------------------------------------
+    let TM1637_CMD1 = 0x40;
+    let TM1637_CMD2 = 0xC0;
+    let TM1637_CMD3 = 0x80;
+    let _SEGMENTS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
+
+    /**
+     * TM1637 LED display
+     */
+    export class TM1637LEDs {
+        buf: Buffer;
+        clk: DigitalPin;
+        dio: DigitalPin;
+        _ON: number;
+        brightness: number;
+        count: number;  // number of LEDs
+
+        /**
+         * initial TM1637
+         */
+        init(): void {
+            pins.digitalWritePin(this.clk, 0);
+            pins.digitalWritePin(this.dio, 0);
+            this._ON = 8;
+            this.buf = pins.createBuffer(this.count);
+            this.clear();
+        }
+
+        /**
+         * Start 
+         */
+        _start() {
+            pins.digitalWritePin(this.dio, 0);
+            pins.digitalWritePin(this.clk, 0);
+        }
+
+        /**
+         * Stop
+         */
+        _stop() {
+            pins.digitalWritePin(this.dio, 0);
+            pins.digitalWritePin(this.clk, 1);
+            pins.digitalWritePin(this.dio, 1);
+        }
+
+        /**
+         * send command1
+         */
+        _write_data_cmd() {
+            this._start();
+            this._write_byte(TM1637_CMD1);
+            this._stop();
+        }
+
+        /**
+         * send command3
+         */
+        _write_dsp_ctrl() {
+            this._start();
+            this._write_byte(TM1637_CMD3 | this._ON | this.brightness);
+            this._stop();
+        }
+
+        /**
+         * send a byte to 2-wire interface
+         */
+        _write_byte(b: number) {
+            for (let i = 0; i < 8; i++) {
+                pins.digitalWritePin(this.dio, (b >> i) & 1);
+                pins.digitalWritePin(this.clk, 1);
+                pins.digitalWritePin(this.clk, 0);
+            }
+            pins.digitalWritePin(this.clk, 1);
+            pins.digitalWritePin(this.clk, 0);
+        }
+
+        /**
+         * set TM1637 intensity, range is [0-8], 0 is off.
+         * @param val the brightness of the TM1637, eg: 7
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_set_intensity" block="%tm|set intensity %val"
+        //% weight=50 blockGap=8
+        //% parts="TM1637"
+        intensity(val: number = 7) {
+            if (val < 1) {
+                this.off();
+                return;
+            }
+            if (val > 8) val = 8;
+            this._ON = 8;
+            this.brightness = val - 1;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * set data to TM1637, with given bit
+         */
+        _dat(bit: number, dat: number) {
+            this._write_data_cmd();
+            this._start();
+            this._write_byte(TM1637_CMD2 | (bit % this.count))
+            this._write_byte(dat);
+            this._stop();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * show a number in given position. 
+         * @param num number will show, eg: 5
+         * @param bit the position of the LED, eg: 0
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_showbit" block="%tm|show digit %num |at %bit"
+        //% weight=90 blockGap=8
+        //% parts="TM1637"
+        showbit(num: number = 5, bit: number = 0) {
+            this.buf[bit % this.count] = _SEGMENTS[num % 16]
+            this._dat(bit, _SEGMENTS[num % 16])
+        }
+
+        /**
+          * show a number. 
+          * @param num is a number, eg: 0
+          */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_shownum" block="%tm|show number %num"
+        //% weight=91 blockGap=8
+        //% parts="TM1637"
+        showNumber(num: number) {
+            if (num < 0) {
+                this._dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                this.showbit(Math.idiv(num, 1000) % 10)
+            this.showbit(num % 10, 3)
+            this.showbit(Math.idiv(num, 10) % 10, 2)
+            this.showbit(Math.idiv(num, 100) % 10, 1)
+        }
+
+        /**
+          * show a hex number. 
+          * @param num is a hex number, eg: 0
+          */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_showhex" block="%tm|show hex number %num"
+        //% weight=90 blockGap=8
+        //% parts="TM1637"
+        showHex(num: number) {
+            if (num < 0) {
+                this._dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                this.showbit((num >> 12) % 16)
+            this.showbit(num % 16, 3)
+            this.showbit((num >> 4) % 16, 2)
+            this.showbit((num >> 8) % 16, 1)
+        }
+
+        /**
+         * show or hide dot point. 
+         * @param bit is the position, eg: 1
+         * @param show is show/hide dp, eg: true
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_showDP" block="%tm|DotPoint at %bit|show %show"
+        //% weight=70 blockGap=8
+        //% parts="TM1637"
+        showDP(bit: number = 1, show: boolean = true) {
+            bit = bit % this.count
+            if (show) this._dat(bit, this.buf[bit] | 0x80)
+            else this._dat(bit, this.buf[bit] & 0x7F)
+        }
+
+        /**
+         * clear LED. 
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_clear" block="clear %tm"
+        //% weight=80 blockGap=8
+        //% parts="TM1637"
+        clear() {
+            for (let i = 0; i < this.count; i++) {
+                this._dat(i, 0)
+                this.buf[i] = 0
+            }
+        }
+
+        /**
+         * turn on LED. 
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_on" block="turn on %tm"
+        //% weight=86 blockGap=8
+        //% parts="TM1637"
+        on() {
+            this._ON = 8;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * turn off LED. 
+         */
+        //% group="7-Segment"
+        //% color=#50A820
+        //% blockId="TM1637_off" block="turn off %tm"
+        //% weight=85 blockGap=8
+        //% parts="TM1637"
+        off() {
+            this._ON = 0;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+    }
+
+    /**
+     * create a TM1637 object.
+     * @param clk the CLK pin for TM1637, eg: DigitalPin.P1
+     * @param dio the DIO pin for TM1637, eg: DigitalPin.P2
+     * @param intensity the brightness of the LED, eg: 7
+     * @param count the count of the LED, eg: 4
+     */
+    //% group="7-Segment"
+    //% color=#50A820
+    //% weight=200 blockGap=8
+    //% blockId="TM1637_create" block="CLK %clk|DIO %dio|intensity %intensity|LED count %count"
+    export function create(clk: DigitalPin, dio: DigitalPin, intensity: number, count: number): TM1637LEDs {
+        let tm = new TM1637LEDs();
+        tm.clk = clk;
+        tm.dio = dio;
+        if ((count < 1) || (count > 5)) count = 4;
+        tm.count = count;
+        tm.brightness = intensity;
+        tm.init();
+        return tm;
+    }
+}
