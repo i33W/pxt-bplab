1c5b67d71de7988e9aded1632232709208f8540c
hwangbomee <88476660+i33W@users.noreply.github.com>
2025-02-11 17:46:20 +0900

    중첩 네임스페이스 테스트 롤백

--- main.ts
+++ main.ts
@@ -6,225 +6,223 @@ namespace BPLAB {
 
     // 그룹 1: LCD 관련
     // -----------------------------------------------------------------------------
-    //% weight=1 color=#0fbc11 icon="\uf0ad"
-    namespace LCD {
-        let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574 
-        let BK: number      // Backlight control
-        let RS: number      // Command/Data selection
+    let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574 
+    let BK: number      // Backlight control
+    let RS: number      // Command/Data selection
 
-        // LCD register setup
-        function setreg(d: number) {
-            pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
-            basic.pause(1)
+    // LCD register setup
+    function setreg(d: number) {
+        pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
+        basic.pause(1)
+    }
+
+    // Send data through I2C bus
+    function set(d: number) {
+        d = d & 0xF0
+        d = d + BK + RS
+        setreg(d)
+        setreg(d + 4)
+        setreg(d)
+    }
+
+    // Send command
+    function cmd(d: number) {
+        RS = 0
+        set(d)
+        set(d << 4)
+    }
+
+    // Send data
+    function dat(d: number) {
+        RS = 1
+        set(d)
+        set(d << 4)
+    }
+
+    // Auto detect LCD address
+    function AutoAddr() {
+        let k = true
+        let addr = 0x20
+        let d1 = 0, d2 = 0
+        while (k && (addr < 0x28)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
         }
+        if (!k) return addr
 
-        // Send data through I2C bus
-        function set(d: number) {
-            d = d & 0xF0
-            d = d + BK + RS
-            setreg(d)
-            setreg(d + 4)
-            setreg(d)
+        addr = 0x38
+        while (k && (addr < 0x40)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
         }
+        if (!k) return addr
+        else return 0
+    }
 
-        // Send command
-        function cmd(d: number) {
-            RS = 0
-            set(d)
-            set(d << 4)
+    /**
+     * Initialize LCD and set I2C address. PCF8574/PCF8574A address is 39/63
+     * @param Addr LCD i2c address, eg: 0, 39, 63. 0 for auto detection
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SET_ADDRESS" block="LCD initialize with Address %addr"
+    //% weight=100 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function LcdInit(Addr: number) {
+        if (Addr == 0) i2cAddr = AutoAddr()
+        else i2cAddr = Addr
+        BK = 8
+        RS = 0
+        cmd(0x33)       // Set 4-bit mode
+        basic.pause(5)
+        set(0x30)
+        basic.pause(5)
+        set(0x20)
+        basic.pause(5)
+        cmd(0x28)       // Set mode
+        cmd(0x0C)
+        cmd(0x06)
+        cmd(0x01)       // Clear screen
+    }
+
+    /**
+     * Display number at specified position on LCD
+     * @param n number to display, eg: 10, 100, 200
+     * @param x LCD column position, eg: 0
+     * @param y LCD row position, eg: 0
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="show number %n|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowNumber(n: number, x: number, y: number): void {
+        let s = n.toString()
+        ShowString(s, x, y)
+    }
+
+    /**
+     * Display string at specified position on LCD
+     * @param s string to display, eg: "Hello"
+     * @param x LCD column position, [0 - 15], eg: 0
+     * @param y LCD row position, [0 - 1], eg: 0
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowString(s: string, x: number, y: number): void {
+        let a: number
+
+        if (y > 0)
+            a = 0xC0
+        else
+            a = 0x80
+        a += x
+        cmd(a)
+
+        for (let i = 0; i < s.length; i++) {
+            dat(s.charCodeAt(i))
         }
+    }
 
-        // Send data
-        function dat(d: number) {
-            RS = 1
-            set(d)
-            set(d << 4)
-        }
+    /**
+     * Turn on LCD
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_ON" block="turn on LCD"
+    //% weight=81 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function on(): void {
+        cmd(0x0C)
+    }
 
-        // Auto detect LCD address
-        function AutoAddr() {
-            let k = true
-            let addr = 0x20
-            let d1 = 0, d2 = 0
-            while (k && (addr < 0x28)) {
-                pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
-                d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
-                pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
-                d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
-                if ((d1 == 7) && (d2 == 0)) k = false
-                else addr += 1
-            }
-            if (!k) return addr
+    /**
+     * Turn off LCD
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_OFF" block="turn off LCD"
+    //% weight=80 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function off(): void {
+        cmd(0x08)
+    }
 
-            addr = 0x38
-            while (k && (addr < 0x40)) {
-                pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
-                d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
-                pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
-                d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
-                if ((d1 == 7) && (d2 == 0)) k = false
-                else addr += 1
-            }
-            if (!k) return addr
-            else return 0
-        }
+    /**
+     * Clear all display contents
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_CLEAR" block="clear LCD"
+    //% weight=85 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function clear(): void {
+        cmd(0x01)
+    }
 
-        /**
-         * Initialize LCD and set I2C address. PCF8574/PCF8574A address is 39/63
-         * @param Addr LCD i2c address, eg: 0, 39, 63. 0 for auto detection
-         */
-        //% group="LCD"
-        //% blockId="I2C_LCD1620_SET_ADDRESS" block="LCD initialize with Address %addr"
-        //% weight=100 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function LcdInit(Addr: number) {
-            if (Addr == 0) i2cAddr = AutoAddr()
-            else i2cAddr = Addr
-            BK = 8
-            RS = 0
-            cmd(0x33)       // Set 4-bit mode
-            basic.pause(5)
-            set(0x30)
-            basic.pause(5)
-            set(0x20)
-            basic.pause(5)
-            cmd(0x28)       // Set mode
-            cmd(0x0C)
-            cmd(0x06)
-            cmd(0x01)       // Clear screen
-        }
+    /**
+     * Turn on LCD backlight
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
+    //% weight=71 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOn(): void {
+        BK = 8
+        cmd(0)
+    }
 
-        /**
-         * Display number at specified position on LCD
-         * @param n number to display, eg: 10, 100, 200
-         * @param x LCD column position, eg: 0
-         * @param y LCD row position, eg: 0
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_SHOW_NUMBER" block="show number %n|at x %x|y %y"
-        //% weight=90 blockGap=8
-        //% x.min=0 x.max=15
-        //% y.min=0 y.max=1
-        //% parts=LCD1602_I2C trackArgs=0
-        export function ShowNumber(n: number, x: number, y: number): void {
-            let s = n.toString()
-            ShowString(s, x, y)
-        }
+    /**
+     * Turn off LCD backlight
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="turn off backlight"
+    //% weight=70 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOff(): void {
+        BK = 0
+        cmd(0)
+    }
 
-        /**
-         * Display string at specified position on LCD
-         * @param s string to display, eg: "Hello"
-         * @param x LCD column position, [0 - 15], eg: 0
-         * @param y LCD row position, [0 - 1], eg: 0
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s|at x %x|y %y"
-        //% weight=90 blockGap=8
-        //% x.min=0 x.max=15
-        //% y.min=0 y.max=1
-        //% parts=LCD1602_I2C trackArgs=0
-        export function ShowString(s: string, x: number, y: number): void {
-            let a: number
+    /**
+     * Shift display left
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHL" block="Shift Left"
+    //% weight=61 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shl(): void {
+        cmd(0x18)
+    }
 
-            if (y > 0)
-                a = 0xC0
-            else
-                a = 0x80
-            a += x
-            cmd(a)
-
-            for (let i = 0; i < s.length; i++) {
-                dat(s.charCodeAt(i))
-            }
-        }
-
-        /**
-         * Turn on LCD
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_ON" block="turn on LCD"
-        //% weight=81 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function on(): void {
-            cmd(0x0C)
-        }
-
-        /**
-         * Turn off LCD
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_OFF" block="turn off LCD"
-        //% weight=80 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function off(): void {
-            cmd(0x08)
-        }
-
-        /**
-         * Clear all display contents
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_CLEAR" block="clear LCD"
-        //% weight=85 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function clear(): void {
-            cmd(0x01)
-        }
-
-        /**
-         * Turn on LCD backlight
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
-        //% weight=71 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function BacklightOn(): void {
-            BK = 8
-            cmd(0)
-        }
-
-        /**
-         * Turn off LCD backlight
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="turn off backlight"
-        //% weight=70 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function BacklightOff(): void {
-            BK = 0
-            cmd(0)
-        }
-
-        /**
-         * Shift display left
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_SHL" block="Shift Left"
-        //% weight=61 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function shl(): void {
-            cmd(0x18)
-        }
-
-        /**
-         * Shift display right
-         */
-        //% group="LCD"
-        //% color=#0fbc11
-        //% blockId="I2C_LCD1620_SHR" block="Shift Right"
-        //% weight=60 blockGap=8
-        //% parts=LCD1602_I2C trackArgs=0
-        export function shr(): void {
-            cmd(0x1C)
-        }
+    /**
+     * Shift display right
+     */
+    //% group="LCD"
+    //% color=#0fbc11
+    //% blockId="I2C_LCD1620_SHR" block="Shift Right"
+    //% weight=60 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shr(): void {
+        cmd(0x1C)
     }
 
     // 그룹 2: 7-Segment 관련
